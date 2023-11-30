6df31864a09dcf724c957c5baf8e0ed4b38dd285
Anton Almqvist <jantonalmqvist@gmail.com>
2023-11-30 20:56:11 +0100

    Day19: calc rounds until build instead

--- day19/main.go
+++ day19/main.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+var DEBUG = false
+
 func Filter[T any](in []T, fn func(T) bool) []T {
 	res := make([]T, len(in))
 	cnt := 0
@@ -28,13 +30,12 @@ type Blueprint struct {
 	robots    [4]int
 	resources [4]int
 	costs     [4][4]int
-	visited   map[int]bool
 }
 
 func assembleBlueprints(in []string) []Blueprint {
 	blueprints := make([]Blueprint, len(in))
 	for i, line := range in {
-		b := Blueprint{visited: make(map[int]bool)}
+		b := Blueprint{}
 		b.robots[0] = 1
 		stuff := strings.Split(line, " ")
 		x, _ := strconv.Atoi(stuff[6])
@@ -54,13 +55,36 @@ func assembleBlueprints(in []string) []Blueprint {
 	return blueprints
 }
 
-func (bp *Blueprint) afford(c int) bool {
-	for i, cx := range bp.costs[c] {
-		if bp.resources[i] < cx {
-			return false
+func (bp *Blueprint) timeToBuild(robot_idx, maxTime int) int {
+	timeToAfford := 0
+	for i, cx := range bp.costs[robot_idx] {
+		// This resource is not needed to build this robot
+		if cx == 0 {
+			continue
+		}
+		// Resource needed but we have no robot to mine it
+		if bp.robots[i] == 0 {
+			return maxTime + 1
+		}
+		minsLeft := 0
+		for cx > bp.resources[i]+bp.robots[i]*minsLeft {
+			minsLeft++
+		}
+		if minsLeft > timeToAfford {
+			timeToAfford = minsLeft
 		}
 	}
-	return true
+	return timeToAfford + 1
+}
+
+func (bp *Blueprint) maxRobotCost(resource int) int {
+	maxCost := 0
+	for i := range bp.costs {
+		if bp.costs[i][resource] > maxCost {
+			maxCost = bp.costs[i][resource]
+		}
+	}
+	return maxCost
 }
 
 func (bp *Blueprint) build(i int) {
@@ -77,35 +101,10 @@ func (bp *Blueprint) deconstruct(i int) {
 	bp.robots[i] -= 1
 }
 
-func (bp *Blueprint) unmine() {
+func (bp *Blueprint) mine(min int) {
 	for i, cx := range bp.robots {
-		bp.resources[i] -= cx
+		bp.resources[i] += min * cx
 	}
-}
-
-func (bp *Blueprint) mine() {
-	for i, cx := range bp.robots {
-		bp.resources[i] += cx
-	}
-}
-
-func (bp *Blueprint) seen(m int) bool {
-	_, ok := bp.visited[m]
-	if !ok {
-		bp.visited[m] = true
-		return false
-	}
-	return true
-}
-
-func (bp *Blueprint) max(i int) int {
-	m := 0
-	for _, v := range bp.costs {
-		if v[i] > m {
-			m = v[i]
-		}
-	}
-	return m
 }
 
 func factorial(i int) int {
@@ -118,64 +117,60 @@ func factorial(i int) int {
 		return 6
 	case 4:
 		return 24
+	// If anything scores above this idk
 	default:
-		return 30
+		return 120
 	}
 }
 
-func buildAndDig(min, maxTime int, earliest, score *int, bp *Blueprint, best map[int]int) map[int]int {
+func buildAndDig(min, maxTime int, score *int, bp *Blueprint) {
 	if min >= maxTime {
 		if bp.resources[3] > *score {
 			*score = bp.resources[3]
 		}
-		return best
+		return
 	}
-	if bp.resources[3] < best[min] {
-		return best
-	}
-	if bp.robots[0] == 0 && min > *earliest {
-		return best
-	}
-	left := maxTime - min
-	if bp.resources[3]+bp.robots[3]*left+factorial(left-1) < *score {
-		return best
-	}
-	if bp.robots[3] == 0 && bp.resources[2]+bp.robots[2]*(left-2)+factorial(left-3) < bp.costs[3][2] {
-		return best
+	timeLeft := maxTime - min
+	if bp.resources[3]+timeLeft*bp.robots[3]+factorial(timeLeft) < *score {
+		return
 	}
 	for i := 3; i >= 0; i-- {
-		if bp.afford(i) {
-			if i == 3 && min < *earliest {
-				*earliest = min
-			}
-			if i == 3 && bp.resources[3] < best[min] {
-				best[min] = bp.resources[3]
-			}
-			bp.mine()
-			bp.build(i)
-			best = buildAndDig(min+1, maxTime, earliest, score, bp, best)
-			bp.deconstruct(i)
-			bp.unmine()
-			if i == 3 {
-				return best
-			}
+		if i == 0 && bp.robots[i] >= bp.maxRobotCost(i) {
+			continue
 		}
+		timeToBuild := bp.timeToBuild(i, maxTime)
+		if timeToBuild > timeLeft {
+			continue
+		}
+		bp.mine(timeToBuild)
+		bp.build(i)
+		buildAndDig(min+timeToBuild, maxTime, score, bp)
+		bp.deconstruct(i)
+		bp.mine(-timeToBuild)
 	}
-	bp.mine()
-	best = buildAndDig(min+1, maxTime, earliest, score, bp, best)
-	bp.unmine()
-	return best
+	bp.mine(timeLeft)
+	buildAndDig(min+timeLeft, maxTime, score, bp)
+	bp.mine(-timeLeft)
+	return
 }
 
 func tryBlueprints(bps []Blueprint, maxTime int) int {
 	p1 := 0
 	for id, bp := range bps {
-		fmt.Printf("------------ Blueprint %d ------------\n", id+1)
+		if DEBUG {
+			fmt.Printf("------------ Blueprint %d ------------\n", id+1)
+			fmt.Println("Costs:")
+			for i, costs := range bp.costs {
+				fmt.Println(i, ":", costs)
+			}
+			fmt.Printf("maxCosts: %d, %d, %d, %d\n", bp.maxRobotCost(0), bp.maxRobotCost(1), bp.maxRobotCost(2), bp.maxRobotCost(3))
+		}
 		score := 0
-		earliest := 0
-		bestAtTime := make(map[int]int)
-		buildAndDig(0, maxTime, &earliest, &score, &bp, bestAtTime)
-		fmt.Println(score)
+		buildAndDig(0, maxTime, &score, &bp)
+		if DEBUG {
+			fmt.Println(bp.resources, bp.robots)
+			fmt.Println(score)
+		}
 		p1 += (id + 1) * score
 	}
 	return p1
@@ -184,12 +179,14 @@ func tryBlueprints(bps []Blueprint, maxTime int) int {
 func tryBlueprints2(bps []Blueprint, maxTime int) int {
 	p1 := 1
 	for id, bp := range bps {
-		fmt.Printf("------------ Blueprint %d ------------\n", id+1)
+		if DEBUG {
+			fmt.Printf("------------ Blueprint %d ------------\n", id+1)
+		}
 		score := 0
-		earliest := 0
-		bestAtTime := make(map[int]int)
-		buildAndDig(0, maxTime, &earliest, &score, &bp, bestAtTime)
-		fmt.Println(score)
+		buildAndDig(0, maxTime, &score, &bp)
+		if DEBUG {
+			fmt.Println(score)
+		}
 		p1 *= score
 	}
 	return p1
@@ -200,7 +197,13 @@ func main() {
 	blueprints := assembleBlueprints(input)
 	part1 := tryBlueprints(blueprints, 24)
 	fmt.Println(part1)
-	fmt.Println("-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-")
-	part2 := tryBlueprints2(blueprints[:3], 32)
+	if DEBUG {
+		fmt.Println("-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-")
+	}
+	newBlueprints := blueprints
+	if len(newBlueprints) > 3 {
+		newBlueprints = newBlueprints[:3]
+	}
+	part2 := tryBlueprints2(newBlueprints, 32)
 	fmt.Println(part2)
 }

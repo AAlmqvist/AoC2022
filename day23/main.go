4435113052cddbf866aec40f5d116d60e0f774c2
Anton Almqvist <jantonalmqvist@gmail.com>
2023-12-02 00:03:11 +0100

    Solved both subtasks for day 23

--- day23/main.go
+++ day23/main.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 )
 
+const DEBUG = false
+
 func Filter[T any](in []T, fn func(T) bool) []T {
 	res := make([]T, len(in))
 	cnt := 0
@@ -23,6 +25,19 @@ func Filter[T any](in []T, fn func(T) bool) []T {
 func readInput(filename string) []string {
 	input, _ := os.ReadFile(filename)
 	return Filter(strings.Split(string(input), "\n"), func(s string) bool { return len(s) > 0 })
+}
+
+func placeElfs(in []string) map[string]elf {
+	pos := make(map[string]elf)
+	for i, row := range in {
+		for j, v := range row {
+			if v == '#' {
+				e := elf{x: j, y: -i}
+				pos[e.hash(0, 0)] = e
+			}
+		}
+	}
+	return pos
 }
 
 type elf struct {
@@ -41,22 +56,9 @@ func dehash(h string) elf {
 	return elf{x: x, y: y}
 }
 
-func placeElfs(in []string) map[string]elf {
-	pos := make(map[string]elf)
-	for i, row := range in {
-		for j, v := range row {
-			if v == '#' {
-				e := elf{x: j, y: -i}
-				pos[e.hash(0, 0)] = e
-			}
-		}
-	}
-	return pos
-}
-
 func proposeMove(e elf, pos map[string]elf, dirs []int) string {
 	ds := []int{-1, 0, 1}
-	mv := true
+	noNeighbors := true
 	for _, dx := range ds {
 		for _, dy := range ds {
 			if dx == 0 && dy == 0 {
@@ -64,82 +66,99 @@ func proposeMove(e elf, pos map[string]elf, dirs []int) string {
 			}
 			_, ok := pos[e.hash(dx, dy)]
 			if ok {
-				mv = false
+				noNeighbors = false
 			}
 		}
 	}
-	if mv {
+	// Don't move if we stand alone
+	if noNeighbors {
 		return e.hash(0, 0)
 	}
 	for _, d := range dirs {
-		var h string
+		canMove := true
 		switch d {
 		// north
 		case 0:
-			h = e.hash(0, 1)
 			for _, dx := range ds {
-				_, ok := pos[e.hash(dx, 1)]
-				if ok {
-					h = ""
+				_, occupied := pos[e.hash(dx, 1)]
+				if occupied {
+					canMove = false
+					break
 				}
+			}
+			if canMove {
+				return e.hash(0, 1)
 			}
 		// south
 		case 1:
-			h = e.hash(0, -1)
 			for _, dx := range ds {
-				_, ok := pos[e.hash(dx, -1)]
-				if ok {
-					h = ""
+				_, occupied := pos[e.hash(dx, -1)]
+				if occupied {
+					canMove = false
+					break
 				}
+			}
+			if canMove {
+				return e.hash(0, -1)
 			}
 		// west
 		case 2:
-			h = e.hash(-1, 0)
 			for _, dy := range ds {
-				_, ok := pos[e.hash(-1, dy)]
-				if ok {
-					h = ""
+				_, occupied := pos[e.hash(-1, dy)]
+				if occupied {
+					canMove = false
+					break
 				}
+			}
+			if canMove {
+				return e.hash(-1, 0)
 			}
 		// east
 		case 3:
-			h = e.hash(1, 0)
 			for _, dy := range ds {
-				_, ok := pos[e.hash(1, dy)]
-				if ok {
-					h = ""
+				_, occupied := pos[e.hash(1, dy)]
+				if occupied {
+					canMove = false
+					break
 				}
 			}
-		}
-		if h != "" {
-			return h
+			if canMove {
+				return e.hash(1, 0)
+			}
 		}
 	}
 	return e.hash(0, 0)
 }
 
-func update(pos map[string]elf, dirs []int) (map[string]elf, []int) {
+func update(pos map[string]elf, dirs []int) (map[string]elf, []int, bool) {
 	newPos := make(map[string]elf)
 	proposed := make(map[string][]string)
 	for _, e := range pos {
 		h := proposeMove(e, pos, dirs)
-		m := proposed[h]
-		proposed[h] = append(m, e.hash(0, 0))
+		proposed[h] = append(proposed[h], e.hash(0, 0))
 	}
 	for h, m := range proposed {
 		if len(m) > 1 {
-			for _, h2 := range m {
-				newPos[h2] = dehash(h2)
+			for _, oldH := range m {
+				newPos[oldH] = dehash(oldH)
 			}
 			continue
 		}
 		newPos[h] = dehash(h)
 	}
+	moved := false
+	for k := range newPos {
+		_, ok := pos[k]
+		if !ok {
+			moved = true
+		}
+	}
 	dirs = append(dirs[1:], dirs[0])
 	if len(pos) != len(newPos) {
 		fmt.Println("------ NUMBER OF ELVES ARE CHANGING -------")
+		fmt.Printf("%d != %d\n", len(pos), len(newPos))
 	}
-	return newPos, dirs
+	return newPos, dirs, moved
 }
 
 func findEdges(pos map[string]elf) (int, int, int, int) {
@@ -181,16 +200,32 @@ func printState(pos map[string]elf) {
 }
 
 func main() {
-	input := readInput("test2.txt")
+	input := readInput("input.txt")
 	elves := placeElfs(input)
 	dirs := []int{0, 1, 2, 3}
-	fmt.Println("INITIAL")
-	printState(elves)
-	for i := 0; i < 10; i++ {
-		elves, dirs = update(elves, dirs)
-		fmt.Println("\nROUND", i+1)
+	if DEBUG {
+		fmt.Println("INITIAL")
 		printState(elves)
 	}
+	for i := 0; i < 10; i++ {
+		elves, dirs, _ = update(elves, dirs)
+		if DEBUG {
+			fmt.Println("\nROUND", i+1)
+			printState(elves)
+		}
+	}
 	x0, xM, y0, yM := findEdges(elves)
-	fmt.Println((yM - y0) * (xM - x0))
+	fmt.Println((yM-y0+1)*(xM-x0+1) - len(elves))
+	// Assume round of no moves did not occur within 10 rounds
+	moved := true
+	round := 10
+	for moved == true {
+		elves, dirs, moved = update(elves, dirs)
+		round++
+		if DEBUG {
+			fmt.Println("\nROUND", round)
+			printState(elves)
+		}
+	}
+	fmt.Println(round)
 }
